43684f3f58e1c648ebc54025605d9ee59e954042
Marrtiinnn <martin.estudios.ifz@gmail.com>
2025-06-24 01:17:11 -0300

    Ejercicios Resueltos exitosamente

--- ejercicios_practica/ejercicio_1.py
+++ ejercicios_practica/ejercicio_1.py
@@ -1,9 +1,9 @@
 # IMPORTANTE: NO borrar los comentarios
 
 class Jugador():
-    def __init__(self, nombre):
+    def __init__(self, nombre, puntaje):
         self.nombre = nombre
-        self.puntaje = 0
+        self.puntaje = puntaje
 
     def obtener_puntaje(self):
         return self.puntaje
@@ -22,13 +22,43 @@ if __name__ == "__main__":
     # Al momento de crear el objeto debe indicar un nombre
     # para el jugador
 
+    print('En la resolucion del ejercicio implemente un sistema de carga de jugadores por teclado ' \
+    'donde se va cargar tambien el puntaje. Luego mediante una iteracion se va mostrar en pantalla ' \
+    'los puntajes de los jugadores ingresados')
+
+    jugadores_varios = []
+
+    while True:
+                nombre_jugador = input('Ingrese el nombre del jugador que desea agregar (si desea parar escriba "stop"): ')
+                if nombre_jugador.lower() == 'stop':
+                    break
+                if not nombre_jugador:
+                    print('Ingresa un valor por favor! ')
+                    continue
+
+                try:
+                    puntaje_jugador = int(input(f'Ingrese el puntaje del jugador {nombre_jugador}: '))
+            
+                except ValueError:
+                    print('Ingrese un valor valido')
+                    continue    
+                
+                nuevo_jugador = Jugador(nombre_jugador, puntaje_jugador)
+                jugadores_varios.append(nuevo_jugador)
+                print('Jugador creado exitosamente! ')
+                print()
+            
 
     # 2) Utilice el método "obtener_puntaje"
     # para leer el puntaje actual del jugador
     # Almacene el puntaje obtenido del método
     # en una variable llamada "puntaje"
-    
-    
+    print()
+    print('Recupeando datos de los jugadores: ')
+    for jugador in jugadores_varios:
+        puntaje = jugador.obtener_puntaje()
+        print(f"El puntaje del jugador {jugador.nombre} es/son {puntaje} puntos")
     # Imprimir en pantalla la variable "puntaje"
-
+    print()
     print("terminamos")
+    print('Gracias')
--- ejercicios_practica/ejercicio_2.py
+++ ejercicios_practica/ejercicio_2.py
@@ -5,8 +5,13 @@ class Jugador():
         self.nombre = nombre
         self.puntaje = 0
 
+    def agregar_puntaje (self, puntos): 
+        self.puntaje += puntos
+
+
     def obtener_puntaje(self):
         return self.puntaje
+    
 
 
 if __name__ == "__main__":
@@ -25,29 +30,54 @@ if __name__ == "__main__":
     # Ver los ejemplos a continuación de lo que
     # se espera al utilizar el método:
 
-    jugador1 = Jugador("Laura")
+    # jugador1 = Jugador("Laura")
+    while True:
+        jugador_1 = input('Ingrese el nombre del jugador que desea agregar: ')
+        if jugador_1 is None:
+            print('Ingresa un nombre por favor !')
+        else:
+            nuevo_jugador = Jugador(jugador_1)
+            break
 
+    puntaje = nuevo_jugador.obtener_puntaje()
+    print(f"*****Al momento de la consulta el puntaje inicial de {jugador_1} es: ", puntaje)
+    print()
+
+    print(f'Vamos a ingresar valores nuevos para {jugador_1}')
+    while True:
+        try:
+            puntos = int(input(f'Ingrese los puntos que desea que se sumen al jugador {jugador_1}: '))
+            break
+        except ValueError:
+            print('!!!!! Ingresa un valor valido !!!!!! ')
+        
+    
+    nuevo_jugador.agregar_puntaje(puntos)
+
+    puntaje_nuevo = nuevo_jugador.obtener_puntaje()
+    print()
+    print(f'El puntaje de {jugador_1} ha sido modificado, el nuevo valor es: ',puntaje_nuevo)
     # Este valor de puntaje debe ser 0
     # Ya que es el valor inicial y no hemos
     # sumado nada hasta ahora
-    puntaje = jugador1.obtener_puntaje()
-    print("Puntaje inicial:", puntaje)
+    # puntaje = jugador_1.obtener_puntaje()
+    # print("Al momento de la consulta el puntaje inicial es: ", puntaje)
 
     # Agregar 10 puntos:
-    jugador1.agregar_puntaje(10)
+    # jugador1.agregar_puntaje(10)
 
     # Este valor de puntaje debe ser 10
     # Ya que hemos agregado 10 puntos
-    puntaje = jugador1.obtener_puntaje()
-    print("Nuevo puntaje del jugador:", puntaje)
+    # puntaje = jugador1.obtener_puntaje()
+    # print("Nuevo puntaje del jugador:", puntaje)
     
     # Agregar 5 puntos:
-    jugador1.agregar_puntaje(5)
+    # jugador1.agregar_puntaje(5)
 
     # Este valor de puntaje debe ser 15
     # Ya que hemos agregado 5 puntos
-    puntaje = jugador1.obtener_puntaje()
-    print("Nuevo puntaje del jugador:", puntaje)
+    # puntaje = jugador1.obtener_puntaje()
+    # print("Nuevo puntaje del jugador:", puntaje)
 
 
     print("terminamos")
